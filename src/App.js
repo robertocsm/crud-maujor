63c2576a8da82f035a91af04b0f646015d07391a
Roberto <cwb.roberto@gmail.com>
2023-12-14 11:10:49 -0300

    commit inicial
    Signed-off-by: Roberto <cwb.roberto@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Menu from "./components/Menu";
+import TabelaLivros from "./components/TabelaLivros";
+import CadastrarLivros from "./components/CadastrarLivros";
+import NotFound from "./components/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    livros: [
+      {
+        id: 1,
+        isbn: "978-85-7522-403-8",
+        titulo: "HTML5 - 2ª Edição",
+        autor: "Maurício Samy Silva",
+      },
+      {
+        id: 2,
+        isbn: "978-85-7522-807-4",
+        titulo: "Introdução ao Pentest",
+        autor: "Daniel Moreno",
+      },
+      {
+        id: 3,
+        isbn: "978-85-7522-780-8",
+        titulo: "Internet das Coisas para Desenvolvedores",
+        autor: "Ricardo da Silva Ogliari",
+      },
+    ],
+  };
+
+  inserirLivro = livro =>{
+    livro.id = this.state.livros.length + 1;
+    this.setState({
+      livros: [...this.state.livros, livro]
+    });
+  };
+
+  render() {
+    return (
+      <Router>        
+        <Menu />
+        <Routes>
+          <Route exact path="/" element={ <TabelaLivros livros={this.state.livros} />} />
+          <Route exact path="/cadastrar" element={ 
+            <CadastrarLivros 
+            inserirLivro = {this.inserirLivro}
+            livro = {{ id: 0, isbn:"", titulo:"", autor:""}}
+            />
+          } 
+          />
+          <Route          
+            path="/editar/:isbnLivro"
+            element={(props) => {
+              const livro = this.state.livros.find(
+                (livro) => livro.isbn === this.props.match.params.isbnLivro
+              );
+              if (livro) {
+                return (
+                  <CadastrarLivros
+                    editarLivro={this.editarLivro}
+                    livro={livro}
+                  />
+                );
+              } else {
+                return <Navigate to="/" />;
+              }
+            }} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
